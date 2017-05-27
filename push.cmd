9f5242a099f3e999682149050872fe4cc8eff416
maxis1314 <maxis1314@163.com>
2017-05-27 16:26:00 +0800

    2_2017/05/27

--- push.cmd
+++ push.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-for /l %%a in (1,1,100) do (
+rem for /l %%a in (1,1,100) do (
 set /p var=ÇëÊäÈëÄÚÈİ:
 git add *
-git commit -m %var%_1
+git commit -m %var%_%date:~0,10%
 git push origin master
 pause
-)
+rem )
