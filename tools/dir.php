540b7e64de9c4294e42c8d4c67a7e2607fd96dd6
maxis1314 <maxis1314@163.com>
2017-06-09 16:50:46 +0800

    1 2017/06/09

--- tools/dir.php
+++ tools/dir.php
@@ -4,20 +4,46 @@ $dir = $argv[1]?$argv[1]:"test";
 $search = $argv[2]?$argv[2]:"";
 $to = $argv[3]?$argv[3]:"";
 $files = listDir($dir);
-echo $dir;
-echo $search;
-echo $to;
+echo 'DIR:',$dir,"\n";
+echo 'Search:',$search,"\n";
+echo 'To:',$to,"\n";
 
 foreach($files['lists'] as $one){
     $subdir = $one['name'];    
     if(strpos($subdir,$search)!== false){    
-        echo $subdir,"\n";
+        
         $subfiles = listDir("$dir/$subdir/");        
         foreach($subfiles['lists'] as $two){
             $subname  = $two['name'];
-            if(strpos($subname,".mp4")!== false){
+            if(strpos($subname,".wmv")!== false){
+                echo "move $dir/$subdir/$subname to $to/$subname\n";
                 rename("$dir/$subdir/$subname", "$to/$subname");
             }
         }
     }
+}
+
+
+function listDir($dir){
+    if(!file_exists($dir)||!is_dir($dir)){
+        return '';
+    }
+    $dirList=array('dirNum'=>0,'fileNum'=>0,'lists'=>'');
+    $dir=opendir($dir);
+    $i=0;
+    while($file=readdir($dir)){
+        if($file!=='.'&&$file!=='..'){
+            $dirList['lists'][$i]['name']=$file;
+            if(is_dir($file)){
+                $dirList['lists'][$i]['isDir']=true;
+                $dirList['dirNum']++;
+            }else{
+                $dirList['lists'][$i]['isDir']=false;
+                $dirList['fileNum']++;
+            }
+            $i++;
+        };
+    };
+    closedir($dir);
+    return $dirList;
 }
