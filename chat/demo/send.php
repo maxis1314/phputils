12c895d6a6e2410240b895e7b724a492e60fb966
maxis1314 <maxis1314@163.com>
2017-07-11 13:47:37 +0800

    1 2017/07/11

--- chat/demo/send.php
+++ chat/demo/send.php
@@ -2,9 +2,9 @@
 //redis_key =  app_name + room_id + username
 
 $send_info = array(
-    "game_name" => $room_info['game_name'],
-    "username" => $room_info['username'],
-    "type" => 'begin_live', #必填
+    "r_id" => 1,
+    "user_name" => 'addd',
+    "type" => 'public_message', #必填
     "time" => date("H:i:s")
 );
 $this->redis->publish('chat-123', json_encode($send_info)); //向34332房间所有人发
